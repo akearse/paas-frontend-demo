0b814907531f058503a087643638ef2d142d4383
akearse <thanagorn.tan@gmail.com>
2022-08-24 15:07:26 +0700

    update dashboard page

--- src/components/pages/DashBoardPage/DashBoardPage.tsx
+++ src/components/pages/DashBoardPage/DashBoardPage.tsx
@@ -1,53 +1,61 @@
 import * as React from "react";
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridColDef,
+  GridRenderCellParams,
+  GridValueGetterParams,
+} from "@mui/x-data-grid";
+import axios from "axios";
+import { imageUrl } from "../../../Constants";
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
   {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
+    field: "image",
+    headerName: "IMAGE",
+    width: 100,
+    renderCell: ({ value }: GridRenderCellParams<string>) => {
+      return (
+        <img
+          src={`${imageUrl}/images/${value}?dummy=${Math.random()}`}
+          style={{ width: 70, height: 70, borderRadius: "5%" }}
+        />
+      );
+    },
   },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
+  { field: "name", headerName: "NAME", width: 400 },
+  { field: "price", headerName: "PRICE", width: 130 },
+  { field: "stock", headerName: "STOCK", width: 130 },
 ];
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
+// const rows = [
+//   { id: 1, name: "Snow", price: "Jon", stock: 35 },
+//   { id: 2, name: "Snow2", price: "Jon", stock: 35 },
+//   { id: 3, name: "Snow3", price: "Jon", stock: 35 },
+// ];
 
 export default function DashBoardPage() {
-  return (
-    // <div style={{ height: 400, width: "100%" }}>
-    //   <DataGrid
-    //     rows={rows}
-    //     columns={columns}
-    //     pageSize={5}
-    //     rowsPerPageOptions={[5]}
-    //     checkboxSelection
-    //   />
-    // </div>
+  const [rows, setRows] = React.useState([]);
 
-    <>
-      <div>DashBaord Page</div>
-    </>
+  React.useEffect(() => {
+    axios.get("http://localhost:8085/api/v2/stock/product").then((result) => {
+      setRows(result.data);
+    });
+  }, []);
+
+  return (
+    <div style={{ height: 500, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={7}
+        rowsPerPageOptions={[7]}
+        checkboxSelection
+      />
+    </div>
+
+    // <>
+    //   <div>DashBoard Page</div>
+    // </>
   );
 }
